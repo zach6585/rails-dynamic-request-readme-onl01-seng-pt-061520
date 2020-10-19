966c901e166d4f2a520d07e303dd110d649577d0
zachary mines <zmines@wesleyan.edu>
2020-10-19 21:25:20 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,3 +12,8 @@ describe 'navigate' do
     expect(page.status_code).to eq(200)
   end
 end
+it 'shows the title on the show page in an h1 tag' do
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("h1", text: "My Post")
+  end
+end
