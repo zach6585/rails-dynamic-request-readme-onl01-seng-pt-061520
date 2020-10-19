808ca22c3a16bd522f88c3c0732ed11c6e2fa0d8
zachary mines <zmines@wesleyan.edu>
2020-10-19 21:27:37 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -16,8 +16,9 @@ describe 'navigate' do
     expect(page).to have_css("h1", text: "My Post")
   end
   it 'shows the description on the show page in a p tag' do
-  visit "/posts/#{@post.id}"
-  expect(page).to have_css("p", text: "My post desc")
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("p", text: "My post desc")
+  end 
 end
 
   
