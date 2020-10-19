5abe71cacb89681101cd1edbdd8c0d8bd75fb312
zachary mines <zmines@wesleyan.edu>
2020-10-19 21:20:32 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,14 @@
+# spec/features/post_spec.rb
+ 
 require 'rails_helper'
-
-describe 'navigate' do 
-  before do 
-    @post = Post.create(title: "My Post", description: "")
+ 
+describe 'navigate' do
+  before do
+    @post = Post.create(title: "My Post", description: "My post desc")
+  end
+ 
+  it 'loads the show page' do
+    visit "/posts/#{@post.id}"
+    expect(page.status_code).to eq(200)
+  end
+end
